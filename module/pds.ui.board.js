06790a6ca8906aeb4dab5aac70189c5979e7d8e6
LeonLi0317 <isu10122006m@cloud.isu.edu.tw>
2025-03-09 01:42:25 +0800

    87787

--- module/pds.ui.board.js
+++ module/pds.ui.board.js
@@ -93,7 +93,7 @@ export default new class Board {
         new Sortable(document.getElementById('board'), {
             animation: 150,
             delay: 300,
-            delayOnTouchOnly: true,
+            delayOnTouchOnly: false,
             touchStartThreshold: 2,
             draggable: ".board-block",
             scroll: true,
